b79290ede6b834e600f5e4afd50e82855a50d328
Isael Gabriel <isaelgsfaria@gmail.com>
2025-10-04 03:29:21 -0300

    Add base player movement

---
+++ scenes/scripts/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+
+const SPEED = 20
+
+var movement_input: Vector2
+
+
+func _process(_delta: float) -> void:
+	_handle_input()
+	
+func _handle_input():
+	movement_input = Input.get_vector("p_left", "p_right", "p_up", "p_down")
+
+func _physics_process(_delta: float) -> void:
+	velocity = Vector3(movement_input.x, 0, movement_input.y) * SPEED
+	
+	move_and_slide()
