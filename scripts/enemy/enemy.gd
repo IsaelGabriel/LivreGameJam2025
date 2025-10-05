567c1f0c2954adfb9c54188f9235e71fb4f57402
Isael Gabriel <isaelgsfaria@gmail.com>
2025-10-05 18:37:01 -0300

    Add game won scene

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -2,3 +2,7 @@ extends Character
 
 func _on_damageable_took_damage() -> void:
 	character_signal.emit('damage')
+
+
+func _on_damageable_has_died() -> void:
+	get_tree().change_scene_to_file("res://scenes/game_won.tscn")
--- scripts/ui/scene_transitioner.gd
+++ scripts/ui/scene_transitioner.gd
@@ -1,7 +1,29 @@
 extends Node
 
+
+@export var wait: float = 3.0
 @export var next_scene: PackedScene
+@export var text: Control
+
+var timer: Timer
+var t: float
+var sine_speed = 1.5
+
+func _ready() -> void:
+	text.visible = false
+	timer = Timer.new()
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer)
+	timer.start(wait)
+
+func _process(delta: float) -> void:
+	t += delta
+	text.modulate.a = sin(t * sine_speed) ** 2
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.is_pressed():
+	if event is InputEventKey and event.is_pressed() and timer.is_stopped():
 		get_tree().change_scene_to_packed(next_scene)
+
+func _on_timer_timeout() -> void:
+	timer.stop()
+	text.visible = true
