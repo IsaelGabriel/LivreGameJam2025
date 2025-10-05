c9db8a3a365bb0d027493f1d273222d848ef3610
Isael Gabriel <isaelgsfaria@gmail.com>
2025-10-05 16:21:34 -0300

    Add item resources

---
+++ scripts/items/item_resource.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name ItemResource
+
+@export var item_name: StringName
+@export var sprite: Texture2D
+@export var spin: bool = false
+@export var collide_with_ball: bool = true
+@export var damage: int = 1
